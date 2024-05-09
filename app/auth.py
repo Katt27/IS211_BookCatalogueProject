363c6e50a8b295c609cb0ba4b84f1623b1902c71
Kathleen <83776807+Katt27@users.noreply.github.com>
2024-05-09 16:49:05 -0400

    Create auth.py

---
+++ app/auth.py
@@ -0,0 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
+
+# Implement functions for user authentication here
